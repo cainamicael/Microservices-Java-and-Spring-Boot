60842d9d05113df0e9261783876e527469d03afb
cainamicael <cainamicaeloficial@gmail.com>
2023-08-01 18:15:18 -0300

    add cloud gateway

---
+++ Project/msclouldgateway/src/main/java/io/github/cainamicael/msclouldgateway/MsclouldgatewayApplication.java
@@ -0,0 +1,28 @@
+package io.github.cainamicael.msclouldgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MsclouldgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclouldgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				//Definindo rotas - Toda vez que a gente entrar nessa rota, vai ser redirecionado para a url (LoadBalancer do micro serviço), com o LoadBalancer
+				.route(r -> r.path("/clientes/**").uri("lb://msclientes")) //O ** quer dizer qq coisa que venha depois e lb é o loadbalancer
+				.build();
+	}
+}
---
+++ Project/msclouldgateway/src/test/java/io/github/cainamicael/msclouldgateway/MsclouldgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cainamicael.msclouldgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclouldgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
