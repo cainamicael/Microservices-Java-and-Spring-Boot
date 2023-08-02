65472f555c03fa2404e7540c76392dd6f1f268b9
cainamicael <cainamicaeloficial@gmail.com>
2023-08-02 11:17:27 -0300

    add controller

--- Project/mscartoes/src/main/java/io/github/cainamicael/mscartoes/application/CartoesResource.java
+++ Project/mscartoes/src/main/java/io/github/cainamicael/mscartoes/application/CartoesResource.java
@@ -1,5 +1,7 @@
 package io.github.cainamicael.mscartoes.application;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.cainamicael.mscartoes.domain.Cartao;
@@ -18,7 +21,7 @@ public class CartoesResource {
 	
 	@Autowired
 	private CartaoService service;
-
+	
 	@GetMapping
 	public String status() {
 		return "OK";
@@ -31,4 +34,9 @@ public class CartoesResource {
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 	
+	@GetMapping(params = "renda")
+	public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda ) {
+		List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+		return ResponseEntity.ok(list);
+	}
 }
